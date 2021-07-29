615c0f90df3ae43f3086e46b8034ef9166a6c340
julia1450 <julia1450@naver.com>
2021-07-29 13:11:08 +0900

    작업물 리스트를 가져오는 API를 등록한다

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World"))
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log(`Example app listenling on port ${port}`));
 
-app.listen(port, () => console.log(`Example app listenling on port ${port}`))
+// 작업물 관련 API를 등록한다
+const works = express();
+app.use('/work', works);
+
+works.get("/", (req, res) => {
+    const start = req.query.start;
+    const size = req.query.size;
+    const workList = [];
+    
+    res.send( workList.slice(start, start + size) );
+});
